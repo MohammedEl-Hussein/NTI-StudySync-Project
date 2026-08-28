f0fa9ad0a7f7e962f0a4e6f67bcace5019cb3765
Zeyad Ahmed <zeyadahmed229@gmail.com>
2026-08-28 18:37:38 +0300

    feat: add dotenv for environment variable management and update MongoDB connection

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const express = require('express');
-
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const mongoose = require('mongoose');
-const port = 3001;
+
+const port = process.env.PORT
 
 app.use(express.json());
-const userRoutes = require('./routes/users');
-app.use("/users",userRoutes);
 
-mongoose.connect('mongodb://127.0.0.1:27017/studySync').then(()=> {
-  console.log('Connected to MongoDB');
-}).catch((err)=> {
-  console.log('Error connecting to MongoDB', err);
-});
-app.listen(port , () => {
-    console.log(`Server is running on port ${port}`);
- });
+const userRoutes = require("./routes/users");
+app.use("/users", userRoutes);
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => {
+        console.log("Connected to MongoDB Atlas");
+        
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.log("Error connecting to MongoDB Atlas:", err.message);
+    });
