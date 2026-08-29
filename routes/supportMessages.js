cdac7db210e7ae0de9a1c4c0d55c26311d6b5c16
salmaehab928-png <salmaehab928@gmail.com>
2026-08-29 18:00:16 +0300

    Refactor routing and authentication for messages and support messages; connect to MongoDB Atlas

--- routes/supportMessages.js
+++ routes/supportMessages.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+const auth = require("../auth/auth");
+// ****const { auth, authorize } = require("../auth/auth");*****
 const router = express.Router();
 
 const {
@@ -10,10 +11,14 @@ const {
   updateSupportMessageStatus,
 } = require("../controllers/supportMessages");
 
-router.post("/", createSupportMessage);
-router.get("/my", getMySupportMessages);
-router.get("/", getAllSupportMessages);
-router.get("/:id", getSupportMessageById);
-router.put("/:id/status", updateSupportMessageStatus);
+
+router.post("/",auth, createSupportMessage);
+router.get("/my", auth, getMySupportMessages);
+router.get("/",auth,getAllSupportMessages);
+//user can see his own message , admin can see all messages
+// ****router.get("/", auth,authorize("admin"),getAllSupportMessages);****
+router.get("/:id",auth,getSupportMessageById);
+//***router.put("/:id/status", auth,authorize("admin") , updateSupportMessageStatus);***
+router.put("/:id/status", auth, updateSupportMessageStatus);
 
 module.exports = router;
