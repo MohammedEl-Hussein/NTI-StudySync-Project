75e78f13967d66982bb012deebbbaf9d2ab2baae
MohammedEl-Hussein <162710648+MohammedEl-Hussein@users.noreply.github.com>
2026-08-31 00:24:35 +0300

    Remove user progress when they leave or are deleted from a room

--- controllers/roomMembers.js
+++ controllers/roomMembers.js
@@ -1,53 +1,9 @@
 const roomMemberModel = require("../models/roomMembers");
 const roomModel = require("../models/rooms");
 const userModel = require("../models/users");
+const progressModel = require("../models/progresses");
 
 
-// const joinRoom = async (req, res) => {
-//     try {
-//         const { roomId } = req.params;
-//         const userId = req.user._id;
-//         // Check if room exists
-//         const room = await roomModel.findById(roomId);
-//         if (!room) {
-//             return res.status(404).json({
-//                 message: "Room not found"
-//             });
-//         }
-//         // Check if user is already a member
-//         const existingMember = await roomMemberModel.findOne({
-//             userId,
-//             roomId
-//         });
-//         if (existingMember) {
-//             return res.status(400).json({
-//                 message: "You are already a member of this room"
-//             });
-//         }
-//         // Check room capacity
-//         const membersCount = await roomMemberModel.countDocuments({
-//             roomId
-//         });
-//         if (membersCount >= room.maxMembers) {
-//             return res.status(400).json({
-//                 message: "Room is full"
-//             });
-//         }
-//         // Create membership
-//         const roomMember = await roomMemberModel.create({
-//             userId,
-//             roomId
-//         });
-//         return res.status(201).json({
-//             message: "Joined room successfully",
-//             roomMember
-//         });
-//     } catch (err) {
-//         return res.status(500).json({
-//             message: err.message
-//         });
-//     }
-// };
 const joinRoom = async (req, res) => {
     try {
         const { roomId } = req.params;
@@ -153,6 +109,12 @@ const leaveRoom = async (req, res) => {
                 message: "You are not a member of this room"
             });
         }
+        
+        // Remove user's progress for this room
+        await progressModel.findOneAndDelete({
+            userId,
+            roomId
+        });
         return res.status(200).json({
             message: "Left room successfully"
         });
@@ -278,6 +240,12 @@ const removeMember = async (req, res) => {
                 message: "User is not a member of this room"
             });
         }
+        
+        // Remove user's progress for this room
+        await progressModel.findOneAndDelete({
+            userId,
+            roomId
+        });
         return res.status(200).json({
             message: "Member removed successfully"
         });
